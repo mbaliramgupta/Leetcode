bc3db3038607f4f2b35b736f82bfe7caa5fb45f3
Baliram Gupta <balramgupta7054@gmail.com>
2025-01-01 09:18:27 +0530

    983_minimumcostfortickets

---
+++ 983_minimumcostfortickets.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=983 lang=cpp
+ *
+ * [983] Minimum Cost For Tickets
+ */
+
+// @lc code=start
+class Solution {
+      public:
+int t[366];
+     int solve(int i,int n,vector<int>&days,vector<int>&costs){
+            if(i>=n){
+                return 0;
+            }
+            if(t[i]!=-1){
+                return t[i];
+            }
+            int cost_1=costs[0]+solve(i+1,n,days,costs);
+
+            int j=i;
+            int max_day=days[i]+7;
+            while(j<n && days[j]<max_day){
+                j++;
+            }
+            int cost_7=costs[1]+solve(j,n,days,costs);
+
+             j=i;
+             max_day=days[i]+30;
+            while(j<n && days[j]<max_day){
+                j++;
+            }
+            int cost_30=costs[2]+solve(j,n,days,costs);
+
+            return t[i]=min({cost_1,cost_7,cost_30});
+     }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+         int n=days.size();
+        memset(t,-1,sizeof(t));
+        return solve(0,n,days,costs);
+    }
+};
+// @lc code=end
