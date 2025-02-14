f872d8db8d2aacaa655f6681d7bd1b1fe75891b6
Baliram Gupta <balramgupta7054@gmail.com>
2025-02-14 12:00:30 +0530

    189.rotate-array

---
+++ 189.rotate-array.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=189 lang=cpp
+ *
+ * [189] Rotate Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        vector<int>temp(k);
+        for(int i=0;i<k;i++){
+             temp[i]=nums[n-k+i];
+        }
+        for(int i=n-1;i>=k;i--){
+            nums[i]=nums[i-k];
+        }
+        for(int i=0;i<k;i++){
+            nums[i]=temp[i];
+        }
+        
+    }
+};
+// @lc code=end
+
